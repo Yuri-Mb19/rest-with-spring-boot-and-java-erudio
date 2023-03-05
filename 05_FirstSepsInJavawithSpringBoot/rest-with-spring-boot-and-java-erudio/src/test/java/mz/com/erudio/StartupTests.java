97063f2c039fad48a35860e610e0f914da5d7b6a
Yuri Mbalane <Yurimbalane19@gmail.com>
2023-03-05 23:00:26 +0200

    First steps in java with spring boot 3

---
+++ 05_FirstSepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/mz/com/erudio/StartupTests.java
@@ -0,0 +1,13 @@
+package mz.com.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
